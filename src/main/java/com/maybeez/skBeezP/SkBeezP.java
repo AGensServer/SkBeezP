c13f9a040396de540d7c8fe78fa4a6be26169db1
Jackwith <86273091+JackwithC2@users.noreply.github.com>
2024-09-17 22:46:06 -0700

    Test

---
+++ src/main/java/com/maybeez/skBeezP/SkBeezP.java
@@ -0,0 +1,17 @@
+package com.maybeez.skBeezP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkBeezP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic2
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
